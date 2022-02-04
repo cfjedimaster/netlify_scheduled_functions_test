d61a021f687e7b6a6cb26c8dec0da090b8127ea3
Raymond Camden <raymondcamden@gmail.com>
2022-02-04 12:54:06 -0600

    misc

--- netlify/functions/rebuild/rebuild.js
+++ netlify/functions/rebuild/rebuild.js
@@ -1,9 +1,14 @@
-const { schedule } = require('@netlify/functions')
+const { schedule } = require('@netlify/functions');
+const fetch = require('node-fetch');
+
+const REBUILD_URL = process.env.rebuildurl;
 
 const handler = async function(event, context) {
     console.log("Received event:", event);
     let body = JSON.parse(event.body);
     console.log(body);
+
+    await fetch(REBUILD_URL, { method: 'POST'});
 
     return {
         statusCode: 200,
